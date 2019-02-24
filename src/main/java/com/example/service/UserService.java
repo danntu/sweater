ca6e279cc1174ca19b5946adcd1bccdf4a265f72
Myrzakanov Daniyar <dan_ntu@mail.ru>
2019-02-24 20:32:50 +0600

    letsCode Spring Boot Jpa (Hibernate): добавляем связи между таблицами базы данных (one to many)

---
+++ src/main/java/com/example/service/UserService.java
@@ -0,0 +1,26 @@
+package com.example.service;
+
+import com.example.repos.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class UserService implements UserDetailsService {
+
+//	private  final UserRepo userRepo;
+//	public UserService(UserRepo userRepo) {
+//		this.userRepo = userRepo;
+//	}
+
+	@Autowired
+	private UserRepo userRepo;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		return userRepo.findByUsername(username);
+	}
+}
