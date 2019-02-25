5157d07f8495c42e7c0ddd5655b53829bb349a6b
Myrzakanov Daniyar <dan_ntu@mail.ru>
2019-02-25 22:09:50 +0600

    logicbig Spring Boot - Starters

--- src/main/java/com/Application.java
+++ src/main/java/com/Application.java
@@ -2,10 +2,31 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.apache.commons.collections.Bag;
+import org.apache.commons.collections.bag.HashBag;
 
 @SpringBootApplication
 public class Application {
+	@Bean
+	public MyBean myBean(){
+		return new MyBean();
+	}
+
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		ApplicationContext context = SpringApplication.run(Application.class, args);
+
+		MyBean bean = context.getBean(MyBean.class);
+		bean.doSomething();
+	}
+
+	private static class MyBean{
+		public void doSomething(){
+			Bag bag = new HashBag();
+			bag.add("ONE", 6);
+			System.out.println("Doing something in MyBean");
+			System.out.println(bag);
+		}
 	}
 }
